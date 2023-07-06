6cc9bfb964c843dea8cca5b422aaedffa21fbdec
Ryan Carpenter <scrian_12@yahoo.com>
2023-07-06 16:08:35 -0400

    added input field and styling

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,17 @@
 import React from "react"
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
 
 const Form = () => {
   return (
-    <section>
-      <form>
-        test
-      </form>
-    </section>
+      <div className="mt-6 flex items-center relative">
+        <input  
+            type="search"
+            id="getword"
+            placeholder="Search for any word..."
+            className="flex align self-start w-full h-12 border-none rounded-[16px] bg-accent text-[1rem] font-bold pl-5 focus:outline-none focus:ring-1 focus:ring-search"
+            />
+            <MagnifyingGlassIcon className="absolute w-6 h-6 text-search right-5 top-3" strokeWidth="1"/>
+      </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,9 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       colors: {
-        secondary: "#006EFF"
+        secondary: "#006EFF",
+        accent: "#f4f4f4",
+        search: "#a445ed"
       }
     },
   },
